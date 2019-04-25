1435bdc66cffaadd228ec7b48af2e9ecd8c0a4b8
symoon94 <msy0128@gmail.com>
2019-04-24 17:03:21 -0700

    mv favicon url to first

--- mukbangtour/mukbangtour/urls.py
+++ mukbangtour/mukbangtour/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth import views
 from django.urls import include, path
 
 urlpatterns = [
+    url(r'^', include('favicon.urls')),
     path('admin/', admin.site.urls),
     url(r'^accounts/login/$', views.LoginView.as_view(template_name="registration/login.html"), name='login'),
     url(r'^accounts/logout/$', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('board.urls')),
-    url(r'^', include('favicon.urls')),
 ]
