b71933ec88569b57a8c4f97ead48682a3e4909fe
symoon94 <msy0128@gmail.com>
2019-04-25 00:34:04 -0700

    add author

--- mukbangtour/board/views.py
+++ mukbangtour/board/views.py
@@ -22,7 +22,8 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=True)
+            post = form.save(commit=False)
+            post.author = request.user
             #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
