8c6e8927309b8460a408e19f26964a45429d491c
symoon94 <msy0128@gmail.com>
2019-04-24 18:14:48 -0700

    rm comment approve

--- mukbangtour/board/views.py
+++ mukbangtour/board/views.py
@@ -82,7 +82,7 @@ def comment_approve(request, pk):
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
-@login_required
+#@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
